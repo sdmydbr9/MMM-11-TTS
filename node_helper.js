abbd3a1a5d73a004b45e6aa109d9107f7db66489
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-11 21:02:06 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,85 +1,21 @@
-var NodeHelper = require("node_helper");
-var request = require("request");
-var fs = require("fs");
-var exec = require("child_process").exec;
+const NodeHelper = require("node_helper");
+const exec = require("child_process").exec;
 
 module.exports = NodeHelper.create({
-  start: function() {
-    console.log("[MMM-11-TTS] Initializing node helper for: " + this.name);
-  },
-
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === "TTS_SAY") {
-      this.getTTS(payload, this.sendResponse.bind(this));
-    }
-  },
-
-  getTTS: function(payload, response) {
-    var self = this;
-    var options = {
-      method: "POST",
-      url: "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM",
-      headers: {
-        "accept": "audio/mpeg",
-        "xi-api-key": "add37c5c39741c022b1cfa0d6af24e0b",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "text": payload.text,
-        "model_id": payload.model_id,
-        "language_id": payload.language_id
-      })
-    };
-
-    console.log("[MMM-11-TTS] Making API call for text: " + payload.text);
-
-    request(options, function(error, apiResponse, body) {
-      if (error) {
-        console.log("[MMM-11-TTS] Error: " + error);
-        response("ERROR");
-        return;
-      }
-
-      if (apiResponse.statusCode === 200) {
-        self.playTTS(payload, response, body);
-      } else {
-        console.log("[MMM-11-TTS] API error: " + apiResponse.statusCode);
-        response("ERROR");
-      }
-    });
-  },
-
-  playTTS: function(payload, response, buffer) {
-  var filename = this.path + "/tmp/" + payload.uid + ".mp3";
-  var self = this;
-
-  fs.writeFile(filename, buffer, function(error) {
-    if (error) {
-      console.log("[MMM-11-TTS] Error: " + error);
-      response("ERROR");
-      return;
-    }
-
-    exec("ffmpeg -i " + filename + " " + filename.replace(".mp3", ".wav"), function (error, stdout, stderr) {
-      if (error) {
-        console.log("[MMM-11-TTS] Error: " + error);
-        response("ERROR");
-        return;
-      }
-
-      self.sendSocketNotification("TTS_FILE", {
-        uid: payload.uid,
-        file: filename.replace(".mp3", ".wav"),
-        type: "wav"
-      });
-
-      response("OK");
-
-      fs.unlink(filename.replace(".mp3", ".wav"), function(error) {
-        if (error) {
-          console.log("[MMM-11-TTS] Error deleting file: " + error);
+    // Define socket notification handler
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "TTS_PLAY") {
+            this.playTTS(payload);
         }
-      });
-    });
-  });
-}
+    },
+
+    // Play TTS function
+    playTTS: function(payload) {
+        let apiKey = this.config.apiKey;
+        let voiceId = this.config.voiceId;
+        let languageId = this.config.languageId;
+        let text = payload.text;
+
+        // Add code to call the TTS API here and play the generated audio
+    }
+});
