271b9f1db38073baa2ab4619b0cfd55f1c356d05
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-11 22:03:06 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -5,10 +5,11 @@ var PythonShell = require("python-shell");
 module.exports = NodeHelper.create({
   start: function() {
     console.log("MMM-11-TTS helper started");
+    this.sendSocketNotification("TTS_PLAY", this.config.startUpMessage);
   },
 
   socketNotificationReceived: function(notification, payload) {
-    if (notification === "TTS_PLAY") {
+    if (notification === "SHOW_ALERT") {
       var options = {
         mode: "text",
         pythonPath: "/usr/bin/python3",
@@ -19,7 +20,7 @@ module.exports = NodeHelper.create({
 
       PythonShell.run("main.py", options, function(err, results) {
         if (err) throw err;
-        console.log("MMM-11-TTS: API call successful");
+        console.log("MMM-11-TTS: TTS API call successful");
       });
     }
   }
