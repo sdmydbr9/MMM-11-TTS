ebf85e87d4f40a721ba7998713c9641b92fcc54a
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-11 22:43:01 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,27 +1,28 @@
-const NodeHelper = require("node_helper");
-var exec = require("child_process").exec;
-var PythonShell = require("python-shell");
+const { spawn } = require("child_process");
 
 module.exports = NodeHelper.create({
   start: function() {
-    console.log("MMM-11-TTS helper started");
-    this.sendSocketNotification("TTS_PLAY", this.config.startUpMessage);
+    console.log(`Starting module helper: ${this.name}`);
   },
 
   socketNotificationReceived: function(notification, payload) {
     if (notification === "SHOW_ALERT") {
-      var options = {
-        mode: "text",
-        pythonPath: "/usr/bin/python3",
-        pythonOptions: ["-u"],
-        scriptPath: "/home/pi/MagicMirror/modules/MMM-11-TTS",
-        args: [payload]
-      };
-
-      PythonShell.run("main.py", options, function(err, results) {
-        if (err) throw err;
-        console.log("MMM-11-TTS: TTS API call successful");
-      });
+      console.log(`Received notification: ${notification}`);
+      this.runScript(payload);
     }
+  },
+
+  runScript: function(payload) {
+    console.log(`Running Python script with text: ${payload.text}`);
+    const process = spawn("python3", ["./main.py", payload.text, payload.voice_id, payload.api_key]);
+    process.stdout.on("data", data => {
+      console.log(`Script output: ${data.toString()}`);
+    });
+    process.stderr.on("data", data => {
+      console.log(`Script error: ${data.toString()}`);
+    });
+    process.on("close", code => {
+      console.log(`Script finished with code: ${code}`);
+    });
   }
 });
