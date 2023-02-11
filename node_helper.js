c0425a472cd8b93c2e515ebcd37d2bc5d37b0280
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-11 16:35:40 +0530

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,13 +1,15 @@
-var NodeHelper = require("node_helper")
-var request = require("request")
+var NodeHelper = require("node_helper");
+var request = require("request");
+var fs = require("fs");
+var exec = require("child_process").exec;
 
 module.exports = NodeHelper.create({
   start: function() {
-    console.log("[MMM-11-TTS] Initializing node helper for: " + this.name)
+    console.log("[MMM-11-TTS] Initializing node helper for: " + this.name);
   },
 
   getTTS: function(payload, response) {
-    var self = this
+    var self = this;
     var options = {
       method: "POST",
       url: "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM",
@@ -21,57 +23,58 @@ module.exports = NodeHelper.create({
         "model_id": payload.model_id,
         "language_id": payload.language_id
       })
-    }
+    };
 
     request(options, function(error, response, body) {
       if (error) {
-        console.log("[MMM-11-TTS] Error: " + error)
-        response.send("ERROR")
-        return
+        console.log("[MMM-11-TTS] Error: " + error);
+        response.send("ERROR");
+        return;
       }
 
       if (response.statusCode === 200) {
-        self.playTTS(payload, response, body)
+        self.playTTS(payload, response, body);
       } else {
-        console.log("[MMM-11-TTS] API error: " + response.statusCode)
-        response.send("ERROR")
+        console.log("[MMM-11-TTS] API error: " + response.statusCode);
+        response.send("ERROR");
       }
-    })
+    });
   },
 
   playTTS: function(payload, response, buffer) {
-    var filename = this.tmpFile + "/" + payload.uid + ".mp3"
-    var self = this
+    var filename = this.tmpFile + "/" + payload.uid + ".mp3";
+    var self = this;
 
     fs.writeFile(filename, buffer, function(error) {
       if (error) {
-        console.log("[MMM-11-TTS] Error: " + error)
-        response.send("ERROR")
-        return
+        console.log("[MMM-11-TTS] Error: " + error);
+        response.send("ERROR");
+        return;
       }
-socketNotificationReceived: function(notification, payload) {
-    if (notification == "TTS_END") {
-        this.sendNotification("TTS_SAY_ENDING", payload);
-
-        // Play the audio as soon as it's received
-        var playCommand = "aplay " + payload;
-        exec(playCommand, function(error, stdout, stderr) {
-            if (error) {
-                console.error(error);
-            }
-        });
-    } else if (notification == "TTS_ERROR") {
-        this.sendNotification("TTS_SAY_ERROR", payload);
-    }
-},
 
       self.sendSocketNotification("TTS_FILE", {
         uid: payload.uid,
         file: filename,
         type: "mp3"
-      })
+      });
 
-      response.send("OK")
-    })
+      response.send("OK");
+    });
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "TTS_END") {
+      this.sendNotification("TTS_SAY_ENDING", payload);
+
+      // Play the audio as soon as it's received
+      var playCommand = "aplay " + payload;
+      exec(playCommand, function(error, stdout, stderr) {
+        if (error) {
+          console.error(error);
+        }
+      });
+    } else if (notification === "TTS_ERROR") {
+      this.sendNotification("TTS_SAY_ERROR", payload);
+    }
   }
-})
+});
