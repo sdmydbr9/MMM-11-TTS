4bb58b78cb4ef6cfeb307b0a7464d087864287d1
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-12 00:23:36 +0530

    Update MMM-11-TTS.js

--- MMM-11-TTS.js
+++ MMM-11-TTS.js
@@ -1,5 +1,5 @@
 Module.register("MMM-11-TTS", {
-    notificationReceived: function(notification, payload) {
+    notificationReceived(notification, payload) {
         if (notification === "SHOW_ALERT") {
             console.log("Received SHOW_ALERT notification: " + payload.message);
         }
