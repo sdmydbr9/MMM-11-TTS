2afebad6fe4ce9b1475ad83ae4c471a559d27078
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-11 23:50:58 +0530

    Update MMM-11-TTS.js

--- MMM-11-TTS.js
+++ MMM-11-TTS.js
@@ -1,25 +1,34 @@
-// Override dom generator.
-getDom: function() {
-    var wrapper = document.createElement("div");
-    return wrapper;
-},
+Module.register("MMM-11-TTS", {
+    // Default config options
+    defaults: {
+        apiKey: "",
+        voiceId: "",
+        startUpMessage: "MMM-11-TTS module has started"
+    },
 
-// Start the module
-start: function() {
-    console.log(this.config.startUpMessage);
-    console.log("Starting module: " + this.name);
-    this.config = Object.assign({}, this.defaults, this.config);
+    // Override dom generator.
+    getDom: function() {
+        var wrapper = document.createElement("div");
+        return wrapper;
+    },
 
-    this.sendSocketNotification("MMM-11-TTS_START", this.config);
-},
+    // Start the module
+    start: function() {
+        console.log(this.config.startUpMessage);
+        console.log("Starting module: " + this.name);
+        this.config = Object.assign({}, this.defaults, this.config);
 
-// Handle notifications
-socketNotificationReceived: function(notification, payload) {
-    console.log("Received socket notification:", notification);
-    if (notification === "SHOW_ALERT") {
-        console.log("Received SHOW_ALERT payload:", payload);
-        var text = payload.message;
-        console.log("SHOW_ALERT message:", text);
-        this.sendSocketNotification("MMM-11-TTS_TEXT", text);
+        this.sendSocketNotification("MMM-11-TTS_START", this.config);
+    },
+
+    // Handle notifications
+    socketNotificationReceived: function(notification, payload) {
+        console.log("Received socket notification:", notification);
+        if (notification === "SHOW_ALERT") {
+            console.log("Received SHOW_ALERT payload:", payload);
+            var text = payload.message;
+            console.log("SHOW_ALERT message:", text);
+            this.sendSocketNotification("MMM-11-TTS_TEXT", text);
+        }
     }
-}
+});
