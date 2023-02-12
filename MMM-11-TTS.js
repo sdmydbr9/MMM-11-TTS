72543081299ff1e5b6c2ec7df7cd2cc85e52c9c4
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-02-12 13:26:16 +0530

    Update MMM-11-TTS.js

--- MMM-11-TTS.js
+++ MMM-11-TTS.js
@@ -1,25 +1,8 @@
 Module.register("MMM-11-TTS", {
-    defaults: {
-    },
-
-    start: function() {
-        console.log(`Starting module: ${this.name}`);
-    },
-
-    getStyles: function() {
-        return [];
-    },
-
-    getDom: function() {
-        var wrapper = document.createElement("div");
-        wrapper.innerHTML = "Hello World!";
-        return wrapper;
-    },
-
-    notificationReceived: function(notification, payload, sender) {
-        if (notification === 'SHOW_ALERT') {
-            console.log(`Received notification: ${notification} from ${sender.name}`);
-            console.log(`Payload: ${JSON.stringify(payload)}`);
-        }
+  // ... other code ...
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "SHOW_ALERT") {
+      console.log("Received socket notification:", payload);
     }
+  },
 });
