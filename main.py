970f1158c5f3e7e5307788de9932e0a1b8c635f2
sdmydbr9 <65818001+sdmydbr9@users.noreply.github.com>
2023-10-23 13:02:19 +0530

    updated to use the new elevenlabs python library

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
 import argparse
-import requests
-import subprocess
+from elevenlabs import generate, play, set_api_key
 
-API_KEY = "api" #from https://beta.elevenlabs.io/speech-synthesis
-VOICE_ID = "EXAVITQu4vr4xnSDxMaL" #any voice you want 
+# Set your API key here
+set_api_key("<YOUR_API_KEY>")
+
+VOICE_ID = "EXAVITQu4vr4xnSDxMaL"  # any voice you want
 
 def execute_script(text):
-    url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}"
-    headers = {
-        "accept": "audio/mpeg",
-        "xi-api-key": API_KEY,
-        "Content-Type": "application/json",
-    }
+    audio = generate(
+        text=text,
+        voice=VOICE_ID,
+        model="eleven_monolingual_v1"
+    )
 
-    data = {
-        "text": text,
-        "model_id": "eleven_monolingual_v1",
-        "language_id": "en_us"
-    }
-
-    response = requests.post(url, headers=headers, json=data)
-
-    if response.status_code == 200:
-        with open("hello_world.mp3", "wb") as f:
-            f.write(response.content)
-            print("Audio generated and saved to hello_world.mp3")
-            subprocess.call(['ffmpeg', '-i', 'hello_world.mp3', 'hello_world.wav'])
-            print("Audio converted to wav format and saved as hello_world.wav")
-            subprocess.call(['aplay', 'hello_world.wav'])
-            print("Audio playing...")
-            subprocess.call(['rm', 'hello_world.mp3'])
-            subprocess.call(['rm', 'hello_world.wav'])
-            print("Files deleted.")
-    else:
-        print("Request failed with status code:", response.status_code)
+    play(audio)
+    print("Audio playing...")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
